d4c1fda7128b970eaabe45550ae8c32a327d7b94
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-21 14:28:53 -0500

    made routine and exercise tables

--- database/dump.sql
+++ database/dump.sql
@@ -16,6 +16,14 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE IF EXISTS ONLY public.routine DROP CONSTRAINT IF EXISTS routine_pkey;
+ALTER TABLE IF EXISTS ONLY public.exercise DROP CONSTRAINT IF EXISTS exercise_pkey;
+ALTER TABLE IF EXISTS public.routine ALTER COLUMN "routineId" DROP DEFAULT;
+ALTER TABLE IF EXISTS public.exercise ALTER COLUMN "exerciseId" DROP DEFAULT;
+DROP SEQUENCE IF EXISTS public."routine_routineId_seq";
+DROP TABLE IF EXISTS public.routine;
+DROP SEQUENCE IF EXISTS public."exercise_exerciseId_seq";
+DROP TABLE IF EXISTS public.exercise;
 DROP EXTENSION IF EXISTS plpgsql;
 DROP SCHEMA IF EXISTS public;
 --
@@ -44,6 +52,132 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: exercise; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.exercise (
+    "exerciseId" integer NOT NULL,
+    name text NOT NULL,
+    description text NOT NULL
+);
+
+
+--
+-- Name: exercise_exerciseId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."exercise_exerciseId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: exercise_exerciseId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."exercise_exerciseId_seq" OWNED BY public.exercise."exerciseId";
+
+
+--
+-- Name: routine; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.routine (
+    "routineId" integer NOT NULL,
+    "userId" integer NOT NULL,
+    "recommendedCalories" integer NOT NULL
+);
+
+
+--
+-- Name: routine_routineId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."routine_routineId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: routine_routineId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."routine_routineId_seq" OWNED BY public.routine."routineId";
+
+
+--
+-- Name: exercise exerciseId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.exercise ALTER COLUMN "exerciseId" SET DEFAULT nextval('public."exercise_exerciseId_seq"'::regclass);
+
+
+--
+-- Name: routine routineId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.routine ALTER COLUMN "routineId" SET DEFAULT nextval('public."routine_routineId_seq"'::regclass);
+
+
+--
+-- Data for Name: exercise; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.exercise ("exerciseId", name, description) FROM stdin;
+\.
+
+
+--
+-- Data for Name: routine; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.routine ("routineId", "userId", "recommendedCalories") FROM stdin;
+\.
+
+
+--
+-- Name: exercise_exerciseId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."exercise_exerciseId_seq"', 1, false);
+
+
+--
+-- Name: routine_routineId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."routine_routineId_seq"', 1, false);
+
+
+--
+-- Name: exercise exercise_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.exercise
+    ADD CONSTRAINT exercise_pkey PRIMARY KEY ("exerciseId");
+
+
+--
+-- Name: routine routine_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.routine
+    ADD CONSTRAINT routine_pkey PRIMARY KEY ("routineId");
 
 
 --
