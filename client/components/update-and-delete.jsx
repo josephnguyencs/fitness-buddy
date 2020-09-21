00a3fd0688abbff6eef33354c5e9d8b3a7f4e3a0
Jake-Mertz <jakemertz101@gmail.com>
2020-09-21 19:28:29 -0400

    update and delete buttons

---
+++ client/components/update-and-delete.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UpdateAndDelete() {
+  return (
+    <div className="container">
+      <button type="button" className="btn btn-primary u-d-buttons">Update</button>
+      <button type="button" className="btn btn-danger u-d-buttons">Delete</button>
+    </div>
+  );
+}
+
+export default UpdateAndDelete;
