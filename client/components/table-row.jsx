731699e6f621afb51bbf948071a6a6feb0f1a6a0
Jake-Mertz <jakemertz101@gmail.com>
2020-09-21 20:34:02 -0400

    a handleclick

--- client/components/table-row.jsx
+++ client/components/table-row.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import UpdateAndDelete from './update-and-delete';
 
-function TableRow() {
-  return (
-    <div className="container">
-      <tbody>
-        <tr>
-          <td>push-ups</td>
-          <UpdateAndDelete />
-        </tr>
-      </tbody>
-    </div>
-  );
+class TableRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routine: []
+    };
+  }
+
+  handleClick() {
+    if (this.event.target.id === 'delete') {
+      fetch('', {
+        method: 'DELETE'
+      })
+        .then(res => res.json())
+        .then(data => this.setState({ routine: data }));
+    } else if (this.event.target.id === 'update') {
+      fetch('', {
+        method: 'PUT'
+      })
+        .then(res => res.json())
+        .then(data => this.setState({ routine: data }));
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <tbody>
+          <tr>
+            <td>push-ups</td>
+            <UpdateAndDelete onClick={this.handleClick}/>
+          </tr>
+        </tbody>
+      </div>
+    );
+  }
 }
 
 export default TableRow;
