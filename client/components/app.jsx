9eaaa320c5a292633f4ded5256fbeb2c2d7ba8e7
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-22 18:01:28 -0500

    show each modal on button click

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from './header';
 import Table from './table';
 import TableDays from './table-days';
+import DefaultAndCustomModal from './default-and-custom-modal';
+import Custom from './custom';
+import DefaultList from './default-list';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: 'table',
       day: '1',
       exercises: [],
       message: null,
@@ -14,6 +18,10 @@ class App extends React.Component {
     };
     this.setDay = this.setDay.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleAddClick = this.handleAddClick.bind(this);
+    this.handleDefaultClick = this.handleDefaultClick.bind(this);
+    this.handleCustomClick = this.handleCustomClick.bind(this);
+    this.handleCancelClick = this.handleCancelClick.bind(this);
   }
 
   componentDidMount() {
@@ -41,14 +49,59 @@ class App extends React.Component {
     this.setExercises(dayId);
   }
 
+  handleAddClick() {
+    this.setState({
+      view: 'choose'
+    });
+  }
+
+  handleDefaultClick() {
+    this.setState({
+      view: 'default'
+    });
+  }
+
+  handleCustomClick() {
+    this.setState({
+      view: 'custom'
+    });
+  }
+
+  handleCancelClick() {
+    this.setState({
+      view: 'table'
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <Header />
-        <TableDays handleClick={this.handleClick}/>
-        <Table exercises={this.state.exercises} />
-      </div>
-    );
+    if (this.state.view === 'table') {
+      return (
+        <div>
+          <Header />
+          <TableDays handleClick={this.handleClick}/>
+          <Table exercises={this.state.exercises} handleClick={this.handleAddClick}/>
+        </div>
+      );
+    } else if (this.state.view === 'choose') {
+      return (
+        <>
+          <Header />
+          <DefaultAndCustomModal handleCancelClick={this.handleCancelClick} handleDefaultClick={this.handleDefaultClick} handleCustomClick={this.handleCustomClick}/>
+        </>
+      );
+    } else if (this.state.view === 'default') {
+      return (
+        <>
+          <DefaultList handleCancelClick={this.handleCancelClick}/>
+        </>
+      );
+    } else if (this.state.view === 'custom') {
+      return (
+        <>
+          <Custom handleCancelClick={this.handleCancelClick}/>
+        </>
+      );
+    }
   }
 }
 
--- client/components/default-list.jsx
+++ client/components/default-list.jsx
@@ -2,39 +2,44 @@ import React from 'react';
 import Header from './header';
 import TableDays from './table-days';
 
-function DefaultList() {
+function DefaultList(props) {
   return (
-    <div className="container">
-      <Header />
-      <TableDays />
-      <div className="card">
-        <div className="card-header text-center mb-2">Add Entry</div>
-        <table className="table table-striped">
-          <tbody>
-            <tr>
-              <td className="col-2">Diamond Pushups</td>
-              <a className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</a>
-            </tr>
-            <tr>
-              <td className="col-2">Diamond Pushups</td>
-              <a className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</a>
-            </tr>
-            <tr>
-              <td className="col-2">Diamond Pushups</td>
-              <a className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</a>
-            </tr>
-            <tr>
-              <td className="col-2">Diamond Pushups</td>
-              <a className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</a>
-            </tr>
-            <tr>
-              <td className="col-2">Diamond Pushups</td>
-              <a className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</a>
-            </tr>
-          </tbody>
-        </table>
+    <>
+      <div className="container">
+        <Header />
+        <TableDays />
+        <div className="card">
+          <div className="card-header text-center mb-2">Add Entry</div>
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <td className="col-2">Diamond Pushups</td>
+                <td className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</td>
+              </tr>
+              <tr>
+                <td className="col-2">Diamond Pushups</td>
+                <td className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</td>
+              </tr>
+              <tr>
+                <td className="col-2">Diamond Pushups</td>
+                <td className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</td>
+              </tr>
+              <tr>
+                <td className="col-2">Diamond Pushups</td>
+                <td className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</td>
+              </tr>
+              <tr>
+                <td className="col-2">Diamond Pushups</td>
+                <td className="btn btn-success btn-secondary align-middles mt-1 mr-2" href="#" role="button">Add</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <div className="row justify-content-center mt-5">
+        <button className="btn btn-danger" onClick={props.handleCancelClick}>Return To Planner</button>
+      </div>
+    </>
   );
 }
 
