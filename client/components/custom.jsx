39f0f2a56a717efce37501a29c078413272aa904
Mansor Almossa <malmossa@gmail.com>
2020-09-22 12:42:11 -0700

    update

---
+++ client/components/custom.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Custom() {
+  return (
+    <div className="card">
+      <div className="card-header">Add Entry</div>
+      <div className="input-group flex-nowrap"></div>
+    </div>
+  );
+}
+
+export default Custom;
