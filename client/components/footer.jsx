e4db3851379af1c4d85564a527d0c0fd47b531f0
Mansor Almossa <malmossa@gmail.com>
2020-09-23 16:06:44 -0700

    footer updated

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="bg-dark footer">
-      <div className="navbar navbar-light bg-dark">
-        <a className="navbar-brand" href="#">
-          <i className="fas fa-calendar-alt fa-2x"></i>
-        </a>
-        <a className="navbar-brand" href="#">
-          <i className="fas fa-dumbbell fa-2x"></i>
-        </a>
-        <a className="navbar-brand" href="#">
-          <i className="fas fa-apple-alt fa-2x"></i>
-        </a>
-      </div>
+
+    <div className="navbar footer">
+      <a className="navbar-brand" href="#">
+        <i className="fas fa-calendar-alt fa-2x"></i>
+      </a>
+      <a className="navbar-brand" href="#">
+        <i className="fas fa-dumbbell fa-2x"></i>
+      </a>
+      <a className="navbar-brand" href="#">
+        <i className="fas fa-apple-alt fa-2x"></i>
+      </a>
     </div>
+
   );
 }
 
