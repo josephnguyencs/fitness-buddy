e5d42bf528083785220914e3d25fc9b5fb2ae6e2
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-23 18:09:17 -0500

    additional work

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -12,7 +12,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'table',
+      view: 'default',
       day: '1',
       exercises: [],
       defaultExercises: [],
