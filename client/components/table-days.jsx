0a0db2afb551e1a06492a13e463a2cdb643267ce
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-25 13:24:35 -0500

    changed day sizing to full width of phone

--- client/components/table-days.jsx
+++ client/components/table-days.jsx
@@ -4,7 +4,7 @@ function TableDays(props) {
 
   return (
     <div className="text-center">
-      <div className="btn-group btn-group-sm mt-2 mb-2">
+      <div className="btn-group btn-group-sm mt-2 mb-2 d-flex">
         <button type="button" className="btn button day-button" id="1" onClick={props.handleClick}>Sun</button>
         <button type="button" className="btn button day-button" id="2" onClick={props.handleClick}>Mon</button>
         <button type="button" className="btn button day-button" id="3" onClick={props.handleClick}>Tue</button>
